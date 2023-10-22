056c6a3bd57a6fdfc48d62b289602ba1dddd3caf
claire <claire0711@gmail.com>
2023-10-22 23:54:38 +0800

    fix backend allow origns url

--- server/main.go
+++ server/main.go
@@ -21,7 +21,8 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:3000",
+		// AllowOrigins: "http://localhost:3000",
+		AllowOrigins: "https://go-react-todo-client.vercel.app",
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 
