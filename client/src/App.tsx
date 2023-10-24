e0494c576da37255cc67df7a2c5f4f4bf826c0cb
claire <claire0711@gmail.com>
2023-10-25 00:07:48 +0800

    fix: frontend call backend url

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,7 @@ export interface Todo {
   done: boolean;
 }
 
-export const ENDPOINT = " http://localhost:4000";
+export const ENDPOINT = " http://backend:4000";
 // export const ENDPOINT = "https://go-react-todo-server.onrender.com";
 
 const fetcher = (url: string) =>
