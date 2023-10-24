57bd9a439f19be606a1ddb36358f80f9e1004044
claireliang <claire.ty.liang@viewsonic.com>
2023-10-24 20:07:41 +0800

    feat: add docker-compose file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,9 +11,8 @@ export interface Todo {
   done: boolean;
 }
 
-// export const ENDPOINT = "http://localhost:4000";
-export const ENDPOINT = "https://go-react-todo-server.onrender.com";
-
+export const ENDPOINT = "http://127.0.0.1:4000";
+// export const ENDPOINT = "https://go-react-todo-server.onrender.com";
 
 const fetcher = (url: string) =>
   fetch(`${ENDPOINT}/${url}`).then((r) => r.json());
