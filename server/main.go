d7ac107b8d688f9237bef5d16087ae3335439963
claireliang <claire.ty.liang@viewsonic.com>
2023-10-24 14:32:12 +0800

    fix: fix cors

--- server/main.go
+++ server/main.go
@@ -21,7 +21,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "https://go-react-todo-client.onrender.com",
+		AllowOrigins: "*",
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 
